77510b4299fc794efef09656f65ac96b1e86b444
junaidkv <jndjkv58@gmail.com>
2021-11-29 15:51:59 +0530

    login added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './login/login.component';
+import { AuthService } from './shared/auth.service';
+import{CoursesService} from './shared/courses.service'
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormsModule,
-    ReactiveFormsModule,
-    HttpClientModule
+   
+    LoginComponent
   ],
   imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    AuthService,
+    CoursesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/shared/jwt-responce.ts
@@ -0,0 +1,11 @@
+export class JwtResponce {
+
+    token:string;
+    UserId:number;
+     UserName:string;
+    Password:string;
+   RoleId:number;
+    IsActive:boolean;
+
+
+}
