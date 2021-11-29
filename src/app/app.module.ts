e7b14add0902b78f62075b50815c94905eeddeb0
sarithsajeev <sarith.sajeev@experionglobal.com>
2021-11-29 16:51:33 +0530

    Commit at 4:50

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,13 +12,15 @@ import { ResourcesComponent } from './resources/resources.component';
 import { ResourceComponent } from './resources/resource/resource.component';
 import { ResourceListComponent } from './resources/resource-list/resource-list.component';
 import { ResourceService } from './shared/resource.service';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoursesComponent } from './courses/courses.component';
 import { AddCourcesComponent } from './add-cources/add-cources.component';
 import { EditCourcesComponent } from './edit-cources/edit-cources.component';
 import { CoursesService } from './shared/courses.service';
 import { LoginComponent } from './login/login.component';
 import { AuthService } from './shared/auth.service';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @NgModule({
   declarations: [
@@ -26,10 +28,12 @@ import { AuthService } from './shared/auth.service';
     LeadComponent,
     ResourcesComponent,
     ResourceListComponent,
+    ResourceEnquiryComponent,
     CoursesComponent,
     AddCourcesComponent,
     EditCourcesComponent,
-    LoginComponent
+    LoginComponent,
+    ResourceComponent
   ],
   imports: [
     ReactiveFormsModule,
@@ -39,7 +43,10 @@ import { AuthService } from './shared/auth.service';
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule,
+    NgxPaginationModule,
+    Ng2SearchPipeModule
   ],
   providers: [
     ResourceService,
